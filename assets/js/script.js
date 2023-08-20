cbce54bec01f2389ee15af420a30b7b9d6c17183
Shivam Phutela <sphutela@contextlogic.com>
2023-08-21 00:35:56 +0530

    final changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -444,7 +444,9 @@
             smartSpeed: 300,
             margin: 30,
             loop:true,
+            nav: true,
             autoplayHoverPause:true,
+            navText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
             responsive: {
                 0 : {
                     items: 1,
